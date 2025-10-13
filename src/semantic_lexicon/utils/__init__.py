c6e02e531c9cff88d1014f0a58c2a2576667e713
Faruk Alpay <32020561+farukalpay@users.noreply.github.com>
2025-10-13 21:58:04 +0200

    Add offline-aware installation resolver

--- src/semantic_lexicon/utils/__init__.py
+++ src/semantic_lexicon/utils/__init__.py
@@ -4,8 +4,16 @@
 
 """Utility helpers for Semantic Lexicon."""
 
+from .install import resolve_package_installation_failure
 from .io import read_jsonl, write_jsonl
 from .random import seed_everything
 from .text import normalise_text, tokenize
 
-__all__ = ["read_jsonl", "write_jsonl", "normalise_text", "tokenize", "seed_everything"]
+__all__ = [
+    "read_jsonl",
+    "write_jsonl",
+    "normalise_text",
+    "tokenize",
+    "seed_everything",
+    "resolve_package_installation_failure",
+]
